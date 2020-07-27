1d9c53a071dc655c0b8de0108f2d3772d38d9128
Gabby Suazo <gabby.suazo@gmail.com>
2020-07-27 16:30:55 -0400

    Style content and edit plugin options

--- main.js
+++ main.js
@@ -1,37 +1,29 @@
 $(document).ready(function(){
-    $('.responsive').slick({
+    $('.center').slick({
+        arrows: true,
+        centerMode: true,
+        centerPadding: '100px',
+        slidesToShow: 3,
         dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
         responsive: [
           {
-            breakpoint: 1024,
+            breakpoint: 768,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
             }
           },
           {
             breakpoint: 480,
             settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
             }
           }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
         ]
       });
   });
